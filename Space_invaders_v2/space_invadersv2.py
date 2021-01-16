326c51418489e9c9182e84596b0218a86578c6ad
JesperVigtel <jesper.hoelland@gmail.com>
2021-01-16 19:28:28 +0100

    ny
    ny

--- Space_invaders_v2/space_invadersv2.py
+++ Space_invaders_v2/space_invadersv2.py
@@ -1,8 +1,6 @@
 import pygame
-from pygame.locals import *
 import random
 from pylab import math
-from pylab import loadtxt
 
 #Starter programmer
 pygame.init()
@@ -10,14 +8,12 @@ pygame.init()
 #Skjermen
 bredde = 800
 høyde = 600
-vindu = pygame.display.set_mode((bredde, høyde), RESIZABLE)
+vindu = pygame.display.set_mode((bredde, høyde))
 pygame.display.set_caption("Space invaders")
 
 
 #Farger
 YELLOW = (255, 255,0)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
@@ -31,9 +27,8 @@ start_bg = pygame.transform.scale(start_bg, (bredde,høyde))
 
 #Lager spiller
 SPILLER = pygame.image.load("./assets/gul-skip.png")
-spillerx = 370
+spillerx = 370  #Startposisjon
 spillery = 470
-vindu.blit(SPILLER, (spillerx, spillery))
 spillerx_forandring = 0
 fart = 8 #Hastighet til spiller
 
@@ -47,35 +42,27 @@ lasery_forandring = 20  #Hastighet til laser
 lasertid = "ready"
 
 #Fienden
-fiende_nummer = 6
-fiende_fart = 1
-ROMVESEN = pygame.image.load("./assets/slemming.png")
-ROMVESEN = pygame.transform.scale(ROMVESEN, (60,60))
-fiendeImg = []
-fiendex = []
+fiende_nummer = 6 #Antall fiender
+fiende_fart = 1 #Start-hastighet fiende
+ROMVESEN = pygame.image.load("./assets/slemming.png")  #Romvesen-figur
+ROMVESEN = pygame.transform.scale(ROMVESEN, (60,60))  #Romvesen størrelse
+fiendeImg = []  #Lister for forskjellige elementer til romvesene. 
+fiendex = [] #Hvert romvesen har hver sitt nummer i listene
 fiendey = []
 fiendex_forandring =[]
 fiendey_forandring = []
 
-"""for i in range(fiende_nummer):
-    fiendeImg.append(ROMVESEN)
-    fiendex.append(random.randint(0, bredde-40))
-    fiendey.append(random.randint(50,150))
-    fiendex_forandring.append(fiende_fart)
-    fiendey_forandring.append(40)
-"""
-
 
  #Score og level
 score_value = 0
-level_nå = 0
+level_nå = 0 #Startlevel
 leveler = 10
 
 
 
-def sjekk_high_score(poeng):  #Funksjon for å lage ny hugh_score på dokument
-    if poeng > int(high_score):
-        fil = open("./assets/poeng.txt","w")
+def sjekk_high_score(poeng):  #Funksjon for å lage ny high_score på dokumentet
+    if poeng > int(high_score):  #Sjekker om poeng er høyere enn på filen
+        fil = open("./assets/poeng.txt","w") #Skriver ny high_score
         fil.write(str(poeng))
         fil.close()
         
@@ -107,7 +94,7 @@ def skriv(t,størrelse,x,y,farge):   #Skriving på skjerm
     tekstRectObj = tekstFlateObj.get_rect()
     tekstRectObj.left = x
     tekstRectObj.top = y
-    vindu.blit(tekstFlateObj, tekstRectObj)  #Sette teksten på skjermen
+    vindu.blit(tekstFlateObj, tekstRectObj)
 
 def ny_runde():  #Lager nye romvesen for hvert "spill"
     fiendeImg.clear()  #Fjerner alt innhold i romvesenlistene
@@ -121,6 +108,12 @@ def ny_runde():  #Lager nye romvesen for hvert "spill"
         fiendey.append(random.randint(50,150))
         fiendex_forandring.append(fiende_fart)
         fiendey_forandring.append(40)
+def start_skjerm():
+     vindu.blit(start_bg, (0,0))
+     skriv("v2",50,600,200,YELLOW)
+     skriv("Av Jesper Vigtel Hølland", 30,550,10,WHITE)
+     skriv("High score: "+high_score, 50,10,10, WHITE)
+     skriv("Obs: Du bruker piltaster til bevegelse og mellomromsttast til skyting", 30,10,570,YELLOW)
     
 
 
@@ -141,15 +134,11 @@ while main:  #Loop for hele programmet
     
     #Introskjerm for spillet
     while intro:
-        vindu.blit(start_bg, (0,0))
-        skriv("v2",50,600,200,YELLOW)
-        skriv("Av Jesper Vigtel Hølland", 30,550,10,WHITE)
-        skriv("High score: "+high_score, 50,10,10, WHITE)
-        skriv("Obs: Du bruker piltaster til bevegelse og mellomromsttast til skyting", 30,10,570,YELLOW)
+        start_skjerm()
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
-            if e.type == pygame.MOUSEBUTTONDOWN:
+            if e.type == pygame.MOUSEBUTTONDOWN:  #Når man presser ned musen avsluttes introen, og spillet starter
                 kjører = True
                 intro = False
                 
@@ -170,13 +159,15 @@ while main:  #Loop for hele programmet
             if e.type == pygame.QUIT:  
                 pygame.quit()
             
-            if e.type == pygame.KEYDOWN:       
-    #Bevegelser for spiller, piltaster
+            if e.type == pygame.KEYDOWN: #Når en knapp trykkes
+                
+                #Bevegelser for spiller, piltaster
                 if e.key == pygame.K_LEFT:  #For bevege deg mot venstre
                     spillerx_forandring = -fart
                 if e.key == pygame.K_RIGHT:#For å bevege deg mot høyre
                    spillerx_forandring = fart
-    #Skyting          
+                   
+                   #Skyting          
                 if e.key == pygame.K_SPACE:
                     if lasertid == "ready":  #Sjekker om laseren er klar for å skytes
                         laserx = spillerx   #finner posisjonen til romskipet
@@ -199,16 +190,16 @@ while main:  #Loop for hele programmet
         for i in range(fiende_nummer):  
             
             if fiendey[i] > 440:   #spillet er over når romvesenet når en viss y-verdi på skjermen
-                for j in range(fiende_nummer):
+                for j in range(fiende_nummer):  #Alle fiendene forsvinner
                     fiendey[j] = 2000
-                    skriv("Game over",100, 200, høyde/2 , YELLOW)
-                    skriv("Press på skjermen for å gå tilbake", 40, 160,400, WHITE)
-                    sjekk_high_score(score_value)  #Sjekker scoren din mot high_scoren
+                skriv("Game over",100, 200, høyde/2 , YELLOW)
+                skriv("Press på skjermen for å gå tilbake", 40, 160,400, WHITE)
+                sjekk_high_score(score_value)  #Sjekker scoren din mot high_scoren
                     
-                    for e in pygame.event.get(): #Avslutter "spillskjermen" og fører deg tilbake til introskjermen
-                        if e.type == pygame.MOUSEBUTTONDOWN:
-                            kjører = False
-                            intro = True
+                for e in pygame.event.get(): #Avslutter "spillskjermen" og fører deg tilbake til introskjermen
+                    if e.type == pygame.MOUSEBUTTONDOWN:
+                        kjører = False
+                        intro = True
                     
              
             fiendex[i] += fiendex_forandring[i]     #Kontinuerlig bevegelse av romvesenet, med en viss fart
@@ -249,11 +240,11 @@ while main:  #Loop for hele programmet
             
         
         #Poeng/score
-        skriv("Level: "+ str(level_nå),40,650,10,WHITE)
+        skriv("Level: "+ str(level_nå),40,650,10,WHITE)  #Forskjellige ting som står på skjermen
         skriv("Poeng: " + str(score_value),40,10,10,WHITE)
-        skriv("High score: "+ high_score, 30,300,10, WHITE)
-        spiller(spillerx,spillery)   
-        pygame.display.update()
+        skriv("High score: "+ high_score, 30,300,10, YELLOW)
+        spiller(spillerx,spillery)   #spiller på skjermen
+        pygame.display.update()  #Oppdatere det som skjer på skjermen
     
     
 
